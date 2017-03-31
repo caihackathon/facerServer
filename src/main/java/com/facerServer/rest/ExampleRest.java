cf4f479842f2308cbf3eda92e60d766e97efb82f
U-CAIHDC\gassg <gassg@7DBL062.caihdc.compaid.com>
2017-03-31 15:59:56 -0500

    New file - ExampleRest.java

---
+++ src/main/java/com/facerServer/rest/ExampleRest.java
@@ -0,0 +1,81 @@
+package com.facerServer.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.facerServer.dao.ApplicationDao;
+import com.facerServer.model.StdApplication;
+
+@Path("example")
+public class ExampleRest {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getText() {
+
+    	List<StdApplication> appList = new ArrayList<StdApplication>();
+    	appList = ApplicationDao.readAll();
+
+    	String msg = new String();    	
+    	
+    	for (StdApplication app : appList) {
+    		
+    		msg = msg + app.getApplicationId() + " - " + app.getApplicationName() + "\n"; 
+    		
+    	}
+    	
+    	return msg;
+    }
+
+    @Path("html")
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public Response getHtml() {
+    	String msg = new String();
+    	msg = "<html> " + "<title>" + "Applications" + "</title><body><h2>Applications</h2>";
+
+    	List<StdApplication> appList = new ArrayList<StdApplication>();
+    	appList = ApplicationDao.readAll();
+    	for (StdApplication app : appList) {
+    		msg = msg + app.getApplicationId() + " - " + app.getApplicationName() + "<br>"; 
+    	}
+    	
+    	msg += "</body></html>";
+    	
+        return Response.status(200).entity(msg).build();
+    }
+
+    @Path("list")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<StdApplication> getList() throws Exception {
+  
+    	ApplicationDao appDao = new ApplicationDao();
+    	List<StdApplication> appList = appDao.readAll();
+
+    	System.out.println("getList(): found " + appList.size() + " message(s) on DB");  	
+    	
+    	return appList;
+    }
+
+    @Path("response")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getResponse() throws Exception {
+    	
+    	List<StdApplication> appList = new ArrayList<StdApplication>();
+    	
+    	appList = ApplicationDao.readAll();
+    	
+    	System.out.println("getResponse(): found " + appList.size() + " message(s) on DB");
+    	
+    	return Response.status(200).entity(appList).build();
+    }
+
+}
